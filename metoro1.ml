c3e2316b22b2fa418027c93ce1a9a862c57f5ec0
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-02-27 03:21:24 +0900

    Data form

--- metoro1.ml
+++ metoro1.ml
@@ -10,21 +10,7 @@ type ekimei_t = {
 type ekikan_t = {
   kiten : string;   (* 起点の駅名 *)
   shuten : string;  (* 終点の駅名 *)
-  keiyu : string;   (* 経由する路線名 *)
-  kyori : float;    (* 2駅間の距離(km) *)
+  keiyu : string;   (* 経由する路線名*)
+  kyori : float;    (* 2駅間の距離(km)*)
   jikan : int       (* 所要時間(分) *)
 }
-
-
-
-(*特定のリストから一部の情報だけ出力を行う*)
-
-let getinfo list = match list with
-    {kanji = k; kana = ka; romaji = r; shozoku = s} -> s ^ "," ^ ka
-        
-
-let sample_data1 = getinfo { kanji = "sample"; kana = "sample1"; romaji = "sample2"; shozoku = "sample4"} = "sample4,sample1"
-let sample_data2 = getinfo { kanji = "sample"; kana = "test"; romaji = "sample2"; shozoku = "test1"} = "test1,test"
-
-
-
