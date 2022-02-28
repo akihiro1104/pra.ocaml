d05104cd78e10f19183301a14d001be32c205183
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-02-28 20:19:20 +0900

    function

--- basic2.ml
+++ basic2.ml
@@ -9,13 +9,13 @@ let test2 = add_to_each 1 [[1];[1]]
 
 
 (*以下がメイン関数なる*)
-(*16行目のprefix_restを変数としてadd_to_each_firstに突っ込む。*)
+(*16行目のprefix_restの変数内に第一引数としてfirst1、第二引数として、prefix restを渡す。*)
 
 let rec prefix list = match list with
  [] -> []
- | first :: rest -> [first] :: add_to_each first (prefix rest)
+ | first1 :: rest -> [first1] :: add_to_each first1 (prefix rest)
 
- let test1 = prefix [1;2;3]
+ let test_1 = prefix [1;2;3]
 
 
 
@@ -83,6 +83,25 @@ let test2 = merge [1;2] [] = [1;2]
 let test3 = merge [] [1;2] = [1;2]
 let test4 = merge [3;7] [1;9] = [1;3;7;9]
 
+
+(* 目的 : 2つのリストを受け取ったら、それらの長さが同じかどうかを判定する *)
+(* equal_length : 'a list -> 'b list -> bool *)
+let rec equal_length lst1 lst2 =
+  match (lst1,lst2) with
+      ([],[]) -> true
+    | (_::_,[]) -> false
+    | ([],_::_) -> false
+    | (_::rest1,_::rest2) -> equal_length rest1 rest2
+
+(* テスト *)
+let test1 = equal_length [] [] = true
+let test2 = equal_length [1] [] = false
+let test3 = equal_length [] [2] = false
+let test4 = equal_length [1;2] [3;4] = true
+let test5 = equal_length [1;2;3] [4;5] = false
+let test6 = equal_length [1;2] [3;4;5] = false
+        
+        
 
 
 
