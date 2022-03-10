8e1971fd231dc5d5d94ac1c904dfe3cbb54da6a6
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-03-10 22:50:16 +0900

    try

--- metoro2.ml
+++ metoro2.ml
@@ -6,6 +6,15 @@ type ekimei_t = {
   shozoku : string; (* 所属する路線名 *)
 }
 
+let ekimei_list = [ 
+{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="丸ノ内線"}; 
+{kanji="新大塚"; kana="しんおおつか"; romaji="shinotsuka"; shozoku="丸ノ内線"}; 
+{kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"}; 
+{kanji="後楽園"; kana="こうらくえん"; romaji="korakuen"; shozoku="丸ノ内線"}; 
+{kanji="本郷三丁目"; kana="ほんごうさんちょうめ"; romaji="hongosanchome"; shozoku="丸ノ内線"}; 
+{kanji="御茶ノ水"; kana="おちゃのみず"; romaji="ochanomizu"; shozoku="丸ノ内線"} 
+] 
+
 let global_ekimei_list = [ 
 {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"}; 
 {kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"}; 
@@ -221,3 +230,30 @@ let test2 = shokika {namae="代々木公園"; saitan_kyori=infinity; temae_list=
           = {namae="代々木公園"; saitan_kyori=infinity; temae_list=[]}
 let test3 = shokika {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[]} "代々木上原"
           = {namae="明治神宮前"; saitan_kyori=infinity; temae_list=[]}
+
+
+
+(*駅名の重複の除法*)
+
+let rec ekimei_insert lst ekimei0 = match lst with 
+    [] -> [ekimei0] 
+  | ({kanji = k; kana = a; romaji = r; shozoku = s} as ekimei) :: rest -> 
+      match ekimei0 with {kanji = k0; kana = a0; romaji = r0; shozoku = s0} -> 
+	if a = a0 then ekimei_insert rest ekimei0 
+	else if a < a0 then ekimei :: ekimei_insert rest ekimei0 
+	else ekimei0 :: lst 
+
+  let rec seiretsu ekimei_list = match ekimei_list with 
+    [] -> [] 
+  | first :: rest -> ekimei_insert (seiretsu rest) first 
+ 
+(* テスト *) 
+let test3 = seiretsu [] = [] 
+let test4 = seiretsu ekimei_list = [ 
+{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="丸ノ内線"}; 
+{kanji="御茶ノ水"; kana="おちゃのみず"; romaji="ochanomizu"; shozoku="丸ノ内線"}; 
+{kanji="後楽園"; kana="こうらくえん"; romaji="korakuen"; shozoku="丸ノ内線"}; 
+{kanji="新大塚"; kana="しんおおつか"; romaji="shinotsuka"; shozoku="丸ノ内線"}; 
+{kanji="本郷三丁目"; kana="ほんごうさんちょうめ"; romaji="hongosanchome"; shozoku="丸ノ内線"}; 
+{kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"} 
+] 
