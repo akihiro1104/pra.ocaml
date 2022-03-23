273a6e88b6b646887c6e8b0a6c8653942ed610ca
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-03-23 14:01:25 +0900

    高階関数リスト処理

--- function_list.ml
+++ function_list.ml
@@ -4,9 +4,35 @@ let is_mod3_1 n = n mod 3 = 1
 
 let rec filter_positive list = match list with
     [] -> []
+    | first :: rest -> let x = filter_positive rest in
+        if is_mod3_1 first then first :: x
+                           else x
+
+let rec pra_1 list = match list with
+    [] -> []
     | first :: rest ->
-        if first > 0 then first :: filter_positive rest 
-                     else filter_positive rest
+        if first > 0 then first :: pra_1 rest
+                     else pra_1 rest
+
+
+(*テストコード*)
+
+let test1_1 = filter_positive [3;4;5;6;7]
+let test1_2 = filter_positive [8766589;723870]
+let test1_3 = pra_1 [3;-1]
+
+
+(*高階関数*)
+let rec filter p list = match list with
+    [] -> []
+    | first :: rest -> if p first then first :: filter p rest
+                                  else  filter p rest
+
+
+
+(*テストコード*)
+
+let test2_1 = filter filter_positive 
 
 
 
