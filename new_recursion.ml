f6e7b278d6963703cc774da8fe9c19f4c584bb48
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-03-26 02:17:46 +0900

    練習に使用、章に関しては理解不能

--- new_recursion.ml
+++ new_recursion.ml
@@ -13,46 +13,52 @@ type about_blood = {
   c_amount : int;
 }
 
+(*各血液型の在庫関連するものを一般化したい。。。*)
 
-let rec b_part_1 list = match list with
+let b_part_1 list = match list with
     [] -> ""
     | { a_amount = a; b_amount = b; c_amount = c;} :: rest -> 
         if a > 0 then "補給が可能です" 
-                  else "補給が不可能です"
+                 else "補給が不可能です"
 
 let b_amout_and_type1 = [{ a_amount=0; b_amount=0; c_amount=0;}]
 let b_amout_and_type2 = [{ a_amount=1; b_amount=0; c_amount=0;}]
 
 
-let rec b_part_2 list = match list with
+let b_part_2 list = match list with
     [] -> ""
     | { a_amount = a; b_amount = b; c_amount = c;} :: rest -> 
         if b > 0 then "補給が可能です" 
-                  else "補給が不可能です"
+                 else "補給が不可能です"
 
 let b_amout_and_type3 = [{ a_amount=0; b_amount=0; c_amount=0;}]
 let b_amout_and_type4 = [{ a_amount=0; b_amount=1; c_amount=0;}]
 
 
-let rec b_part_3 list = match list with
+let b_part_3 list = match list with
     [] -> ""
     | { a_amount = a; b_amount = b; c_amount = c;} :: rest -> 
         if c > 0 then "補給が可能です" 
-                  else "補給が不可能です"
+                 else "補給が不可能です"
                   
 let b_amout_and_type5 = [{ a_amount=0; b_amount=0; c_amount=0;}]
 let b_amout_and_type6 = [{ a_amount=0; b_amount=0; c_amount=1;}]
 
+(*各血液型の在庫状況に対応した文字列の吐き出し*)
 
-
-(*各血液型の在庫状況に対応した文字列の吐き出し
-*)
 let test1_1 = b_part_1 b_amout_and_type1 = "補給が不可能です"
 let test1_2 = b_part_1 b_amout_and_type2 = "補給が可能です"
 let test1_3 = b_part_2 b_amout_and_type3 = "補給が不可能です"
 let test1_4 = b_part_2 b_amout_and_type4 = "補給が可能です"
 let test1_5 = b_part_3 b_amout_and_type5 = "補給が不可能です"
 let test1_6 = b_part_3 b_amout_and_type6 = "補給が可能です"
+
+
+(*メイン*)
+
+
+
+
 
 
 
