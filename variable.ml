4fb5d84a5786a45add27642eaf99d550af98ead0
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-04-17 22:56:15 +0900

    if学習中

--- variable.ml
+++ variable.ml
@@ -8,11 +8,17 @@ let test = "test"
 
 (*関数の定義も可能となっている。*)
 
-let function_test test_n = test_n + 50
-
-let test1 = function_test 50
+let function_test test_n = test_n 
 
 
 
+(*IF文の基本的な構成は以下のようになる*)
+(*事前に変数を作成して、IF文の中で使用することが可能となっている*)
+
+let if_sentence_test test = 
+    if test = 10 then "test1"
+                             else "test2" 
 
 
+let test1 = if_sentence_test 10 = "test1"
+let test2 = if_sentence_test 9 = "test2"
