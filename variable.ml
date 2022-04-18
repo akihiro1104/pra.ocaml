4ddbea5c01938877b273197e2ace3402d4829cec
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-04-18 22:09:54 +0900

    第七章からスタート

--- variable.ml
+++ variable.ml
@@ -17,8 +17,38 @@ let function_test test_n = test_n
 
 let if_sentence_test test = 
     if test = 10 then "test1"
-                             else "test2" 
+                 else "test2" 
+
 
 
 let test1 = if_sentence_test 10 = "test1"
 let test2 = if_sentence_test 9 = "test2"
+
+
+(*受け取った数値が１５以上、２５未満であれば、快適、それ以外は、普通を返す関数*)
+
+let kion n = 
+    if 15 < n && n <= 25 then "快適"
+                         else "普通" 
+
+let test1 = kion 10 = "普通"
+let test2 = kion 16 = "快適"
+let test3 = kion 28 = "普通"
+
+
+
+(*視力を入力して各数値に対してAからDまでのランクを示す*)
+
+let eye_power x =
+    if x >= 2.0 then "AA" 
+                else if x >= 1.0 then "A" 
+                                 else if x >= 0.5 then "B"
+                                                  else if x >= 0.1 then "C" else "D"
+
+let test1_1 = eye_power 4.0 = "AA"
+let test1_2 = eye_power 1.3 = "A"
+let test1_3 = eye_power 0.8 = "B"
+let test1_4 = eye_power 0.4 = "C"
+let test1_5 = eye_power 0.01 = "D"
+
+
