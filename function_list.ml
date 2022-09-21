9de3355d0ed3d59c87276bf252b3a8b13b7dcc36
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-09-22 00:05:50 +0900

    test

--- function_list.ml
+++ function_list.ml
@@ -1,11 +1,7 @@
 (*高階関数を使用したリスト処理*)
 
 
-let rec filter_positive lst = match lst with
-    [] -> []
-    | first :: rest ->
-        if first > 0 then first :: filter_positive rest
-                     else filter_positive rest
+
 
 
     
