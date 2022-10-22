b11d3260466aae02828b8023d7c8b4ed86820aca
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-10-22 23:46:15 +0900

    簡易的な一般化

---
+++ higher_order_function/practice1.ml
@@ -0,0 +1,11 @@
+(*一般化と高階関数*)
+(*一般化は似てる関数を複数作ることを避ける為に、複数の引数を意図的に設定する。↓*)
+
+
+let rec scholarship_count n1 n2 n3 =                   (*n1=支払い額*)
+    if n1 > 16000 then (n1 - 16000) * n2              (*n2=支払い回数*)
+                   else n3 / n1                        (*n3=奨学金*)
+
+
+let test1 = scholarship_count 16000 240 3800000 
+let test2 = scholarship_count 17000 240 3800000
