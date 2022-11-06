1583d8a69d4e0b938fa7c29454240c42abbc6c42
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-11-07 00:22:14 +0900

    関数テストの為のデータ型の導入が未完成

--- metoro/metoro6.ml
+++ metoro/metoro6.ml
@@ -17,11 +17,34 @@ let list = [eki1; eki2; eki3; eki4]
 
 
 (*最小点を未確定集合から取り出し*)
-let rec minimam list = match list with
-    [] -> { namae = ""; saitan_kyori = infinity; temae_list = []}
-    | ({ saitan_kyori = sa1 } as first) :: rest -> match minimam rest with ({ saitan_kyori = sa2 } as v) ->
-         if sa1 < sa2 then first 
-                    else v
+(*リストの更新*)
+(*二つの処理を行う。*)
+
+let rec saitan_wo_bunri list = match list with
+    [] -> ({namae = ""; saitan_kyori = infinity; temae_list = []}, []) 
+    | first :: rest -> let (p,v) = saitan_wo_bunri rest in match (p,v) with 
+        ({namae = fn; saitan_kyori = fs; temae_list = ft}, 
+	    {namae = sn; saitan_kyori = ss; temae_list = st}) -> 
+            if sn = "" then (first,v)
+                       else if fs < ss then (first,p::v)
+                                       else (p,first::v)
+
+
+
+
+
+
+(* 駅の例 *) 
+let eki1 = {namae="池袋"; saitan_kyori = infinity; temae_list = []} 
+let eki2 = {namae="新大塚"; saitan_kyori = 1.2; temae_list = ["新大塚"; "茗荷谷"]} 
+let eki3 = {namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]} 
+let eki4 = {namae="後楽園"; saitan_kyori = infinity; temae_list = []} 
+ 
+(* 駅リストの例 *) 
+let lst = [eki1; eki2; eki3; eki4] 
+ 
+(* テスト *) 
+let test = saitan_wo_bunri lst = (eki3, [eki1; eki2; eki4]) 
 
 let test1 = minimam list = eki3
 
