f4cd685839c8b5a24916720f885a909aa244cef4
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-11-07 18:18:49 +0900

    まったく理解がおいつかない

--- metoro/metoro6.ml
+++ metoro/metoro6.ml
@@ -1,4 +1,5 @@
 (*最短距離最小の点の分離、15.6*)
+(*まったくわからん*)
 
 type eki_t = {
   namae: string;
@@ -21,13 +22,14 @@ let list = [eki1; eki2; eki3; eki4]
 (*二つの処理を行う。*)
 
 let rec saitan_wo_bunri list = match list with
-    [] -> ({namae = ""; saitan_kyori = infinity; temae_list = []}, []) 
-    | first :: rest -> let (p,v) = saitan_wo_bunri rest in match (p,v) with 
+    [] -> ({namae = ""; saitan_kyori = infinity; temae_list = []}, [])           (*最終的に吐き出しさせたいデータ形式の確定*)
+    | first :: rest -> let (p,v) = saitan_wo_bunri rest in match (first,p) with 
         ({namae = fn; saitan_kyori = fs; temae_list = ft}, 
 	    {namae = sn; saitan_kyori = ss; temae_list = st}) -> 
             if sn = "" then (first,v)
                        else if fs < ss then (first,p::v)
                                        else (p,first::v)
+
 
 
 
@@ -41,12 +43,14 @@ let eki3 = {namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]}
 let eki4 = {namae="後楽園"; saitan_kyori = infinity; temae_list = []} 
  
 (* 駅リストの例 *) 
-let lst = [eki1; eki2; eki3; eki4] 
+let lst1 = [eki1; eki2; eki3; eki4] 
  
 (* テスト *) 
-let test = saitan_wo_bunri lst = (eki3, [eki1; eki2; eki4]) 
+let test1 = saitan_wo_bunri lst1 = (eki3, [eki1; eki2; eki4]) 
+let test2 = saitan_wo_bunri lst1
 
-let test1 = minimam list = eki3
+
+
 
 
 
