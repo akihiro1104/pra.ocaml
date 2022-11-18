ccc70bcb3605b3813735b79a1928a33284cfc7d8
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-11-18 17:24:53 +0900

    完了

--- metoro/13/metoro1.ml
+++ metoro/13/metoro1.ml
@@ -385,9 +385,10 @@ let eki4 = {namae="後楽園"; saitan_kyori = infinity; temae_list = []}
 let koushin1 p q = match (p,q) with
     | ({ namae = n1; saitan_kyori = s1; temae_list = te1 } ,
     { namae = n2; saitan_kyori = s2; temae_list = te2 }) ->
-        if hojo_koushin n1 n2 global_ekikan_list = infinity then q
-                                                            else if hojo_koushin n1 n2 global_ekikan_list +. s1 < s1 then {namae = n2; saitan_kyori = s1 +. hojo_koushin n1 n2 global_ekikan_list ; temae_list = n2 :: te1} 
-                                                                                                                     else q
+        let kyori = hojo_koushin n1 n2 global_ekikan_list in
+        if kyori = infinity then q
+                            else if s1 +. kyori < s2 then {namae = n2; saitan_kyori = s1 +. kyori ; temae_list = n2 :: te1} 
+                                                     else q
 
 (*テストコード*)
 let test5 = koushin1 eki3 eki1 = eki1 
@@ -396,7 +397,27 @@ let test7 = koushin1 eki3 eki3 = eki3
 let test8 = koushin1 eki3 eki4 = 
 	{namae="後楽園"; saitan_kyori = 1.8; temae_list = ["後楽園"; "茗荷谷"]} 
 let test9 = koushin1 eki2 eki1 = 
-	{namae="池袋"; saitan_kyori = 3.0; temae_list = ["池袋"; "新大塚"; "茗荷谷"]} 
+	{namae="池袋"; saitan_kyori = 3.0; temae_list = ["池袋"; "新大塚"; "茗荷谷"]}
 let test10 = koushin1 eki2 eki2 = eki2 
-let test11 = koushin1 eki2 eki3 = eki3 
+let test11 = koushin1 eki2 eki3 = eki3  
 let test12 = koushin1 eki2 eki4 = eki4 
+
+
+
+let eki1 = {namae="池袋"; saitan_kyori = infinity; temae_list = []} 
+let eki2 = {namae="新大塚"; saitan_kyori = 1.2; temae_list = ["新大塚"; "茗荷谷"]} 
+let eki3 = {namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]} 
+let eki4 = {namae="後楽園"; saitan_kyori = infinity; temae_list = []} 
+ 
+(* 駅リストの例 *) 
+let lst = [eki1; eki2; eki3; eki4] 
+
+(* 目的：直前に確定した駅 p と、未確定の駅のリスト v を受け取り、 *)
+(* 必要な更新処理を行った後の未確定の駅のリストを返す *)
+
+let koushin p v = let f q = koushin1 p q in List.map f v
+
+let test1 = koushin eki2 [] = [] 
+let test2 = koushin eki2 lst = 
+ [{namae="池袋"; saitan_kyori = 3.0; temae_list = ["池袋"; "新大塚"; "茗荷谷"]}; 
+  eki2; eki3; eki4] 
