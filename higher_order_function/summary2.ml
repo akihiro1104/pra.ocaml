1924d81f162b19b72316ed11c52b22bab8a0a862
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-12-01 06:41:08 +0900

    MAP関数の大まかな処理は理解

--- higher_order_function/summary2.ml
+++ higher_order_function/summary2.ml
@@ -7,6 +7,9 @@
 
 (*上記のふたつの関数の一般化*)
 (*リストに要素が含まれていなかった時に何かしらの吐き出しと停止線が同じことに着目する。*)
+(*List.fold_right関数の使用は似たような処理が複数ある時に行うのが適切かも？*)
+(*上記の関数使用すると引数を複数（アキュムレーター）を設定する必要があり*)
+
 
 (*一般化*)
 let rec fold_right f list init = match list with
@@ -14,12 +17,12 @@ let rec fold_right f list init = match list with
     | first :: rest -> f first (fold_right f rest init)
 
 
-
-(*firstとrest_resultを足す*)
-let add_int first rest_result = first + rest_result
-
 (*最終関数*)
-let sum list = fold_right add_int list 0
+(*rest_resultは途中の計算経過のデータを格納するために、引数に０を設定して関数を動かす*)
+(*以下の構成が、List.fold_right型では定型になるのかな？*)
+
+let sum list = let add_int first rest_result = first + rest_result in
+    fold_right add_int list 0
 
 (*テストコード*)
 let test1_sum = sum [1;2;3;4;5] = 15
