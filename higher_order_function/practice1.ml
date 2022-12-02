180ecd7bdf64b970ea364a4c4d0c2e08e00cbaff
akihiro tsujii <akihiro.tsujii0488@gmail.com>
2022-12-02 22:21:48 +0900

    ネスト練習

--- higher_order_function/practice1.ml
+++ higher_order_function/practice1.ml
@@ -1,45 +1,45 @@
 (*一般化と高階関数*)
 (*練習用ファイル↓*)
 
-let rec sum_1 f list = match list with
-    [] -> []
-    | first :: rest -> f first :: sum_1 f rest
 
-(*名前のない関数*)
-(*上記の関数と合わせて名前のない関数で定めると以下のようになる*)
-(*関数構成は、fun＋引数*)
+(*数値が格納されているリストを受け取ったら、昇降順で並べる*)
+(*真の時に吐き出すリスト構成の時に、再帰構成ではなく引数として与えられているリストをセットする*)
 
-(*関数なし定義*)
-(*名前のない関数は関数を定めず、引数の設定だけを行い*)
-
-(*let sum_main list = sum_1 (fun first -> first + 1) list *)
-
-(*全ての関数定義*)
-let rec f_1 first = first + 1 
-
-(*以下の関数のネスト部分は、名前のない関数を使用せずに定めている。*)
-let sum_main list = List.map f_1 list
-
-let test1_sum = sum_main [] = []
-let test2_sum = sum_main [1;2;3;4;5] = [2;3;4;5;6]
-let test3_sum = sum_main [5;6;7;8;9] = [6;7;8;9;10]  
-
-
-
-(*fold_right練習*)
-(*リストの中をまとめ上げる時には、無理に上記の関数を使用する必要はないかな*)
-
-let rec sum list = match list with
-    [] -> 0
-    | first :: rest -> first + sum rest
+let rec part number list = match list with
+    [] -> [number]
+    | first :: rest -> if number <= first then number :: list
+                                          else first :: part number rest
 
 (*テストコード*)
-let test1_sum = sum [1;2;3;4;5] = 15
-let test2_sum = sum [] = 0
+let test1 = part 1 [] = [1]
+let test2 = part 5 [2;3;4;6]
+let test3 = part 3 [2;7;8;9]
+
+
+(*関数のネスト時、引数構成と引数として与えるデータ構成について注意が必要*)
+let rec sort list = match list with
+    [] -> []
+    | first :: rest -> part first (sort rest)
+
+
+(*テストコード*)
+
+let test1 = sort [6;5;4]
+let test2 = sort [9;8;7;6;5;4;3;2;1]
 
 
 
 
 
 
+(*二つのリストを一つにして、昇降順にならべる*)
 
+let rec duble_sort list1 list2 = match (list1,list2) with
+    ([],[]) -> []
+    | ( first1 :: rest1, [] ) -> [list1]
+    | ( [], first2 :: rest2 ) -> [list2]
+    | ( first1 :: rest1, first2 :: rest2 ) ->
+        if first1 < first2 then first1 :: duble_sort rest1 list2 
+                           else first2 :: duble_sort list1 rest2
+
+let test1 = duble_sort [3;2;1] [6;5;4]
